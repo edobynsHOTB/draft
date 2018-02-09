69ba7739a174ee73236c001d0fb37b8a3f849b00
HOTB Software <edobyns@hotbsoftware.com>
2018-02-09 10:36:23 -0800

    123

--- src/web/src/app.js
+++ src/web/src/app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello Website!'))
+app.get('/', (req, res) => res.send('Hello Website! Woo!'))
 
 app.listen(80, () => {
     console.log('Server listening on port 80')

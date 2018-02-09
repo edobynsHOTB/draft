176ea0b49637640d8fd864f2ebb81f8354ed80e0
noel.eom <noel.eom@NOELEOM-PC>
2018-02-08 17:58:08 -0800

    test push 2

---
+++ src/server/src/models/tempSmsCode.js
@@ -0,0 +1,18 @@
+﻿'use strict';
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+
+var tempSmsCode = new Schema({
+
+        phone: {
+            type: String
+        },
+        smsCode: {
+            type: String
+        }
+}, {
+        collection: 'tempSmsCode'
+    });
+
+module.exports = mongoose.model('tempSmsCode', tempSmsCode);
